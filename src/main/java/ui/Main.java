aa8cdb238645d00916be27c69f79fb0103f35522
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-11-27 23:15:01 +0800

    ui 1

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ServiceToOut serviceToOut=new ServiceToOut();
+        serviceToOut.service();
     }
 }
--- src/main/java/ui/ServiceToOut.java
+++ src/main/java/ui/ServiceToOut.java
@@ -1,13 +1,56 @@
 package ui;
 
+import dao.*;
+import object.Customer;
+import object.Flight;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ServiceToOut {
     public void service(){
+        printWelcomeMenu();
 
+        Scanner sc=new Scanner(System.in);
+        //dao's managers
+        BusManager busManager=new BusManager();
+        CustomerManager customerManager=new CustomerManager();
+        FlightManager flightManager=new FlightManager();
+        HotelManager hotelManager=new HotelManager();
+        ReservationManager reservationManager=new ReservationManager();
+
+        while(true){
+            System.out.println("\n");
+            printMainMenu();
+            //input
+            String choice0=sc.nextLine();
+            System.out.println("");
+
+            switch (choice0){
+                case "1":
+                    ArrayList<Flight> flights=flightManager.searchAllFlight();
+                    System.out.println("Result:");
+                    for(Flight f:flights){
+                        System.out.println(f);
+                    }
+                    break;
+
+            }
+            //一次事务结束
+            System.out.print("\nInput Any Character To Continue:");
+            sc.nextLine();
+        }
     }
 
-    private void printMenu(){
+    private void printWelcomeMenu(){
         System.out.println("" +
                 "TravelManager\n" +
+                "Produced by DeathWind");
+    }
+
+    private void printMainMenu(){
+        System.out.print("" +
+                "* Main Menu *\n" +
                 "---\n" +
                 "1: Search Flights\n" +
                 "2: Add New Flight\n" +
@@ -25,6 +68,13 @@ public class ServiceToOut {
                 "11: Add New Customer\n" +
                 "12: Modify Customer\n" +
                 "---\n" +
-                "");
+                "13: Search Reservation\n" +
+                "14: Add Reservation\n" +
+                "15: Search Own Journey\n" +
+                "---\n" +
+                "16: Check Accessible\n" +
+                "17: QUIT\n" +
+                "\n" +
+                "Input Your Choice: ");
     }
 }
